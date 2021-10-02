efe7c5dbc4745bd6e134976cc20b09c3c5092009
nickguerra <nickg@maliciousgames.com>
2021-10-02 11:58:04 -0500

    Initial Commit

---
+++ BookClub/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult BookList()
+        {
+            return View();
+        }
+    }
+}
