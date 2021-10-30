f697087ec42ffd30704f7912a0f4cd7e72bfeebd
nickguerra <nickg@maliciousgames.com>
2021-10-30 06:01:16 -0500

    Added stubs for Author Api controller

--- BookClub/Controllers/api/AuthorApiController.cs
+++ BookClub/Controllers/api/AuthorApiController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using BookClub.Data;
+using BookClub.Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +16,71 @@ using System.Threading.Tasks;
 namespace BookClub.Controllers.api
 {
     [Route("api/Author")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+   // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [ApiController]
     public class AuthorApiController : ControllerBase
     {
-        
+        private readonly BookClubContext _context;
+        private readonly IAuthorRepository _repository;
+        private readonly ILogger<AuthorApiController> _logger;
+        private readonly IMapper _mapper;
+        private readonly SignInManager<LoginUser> _signInManager;
+        private readonly UserManager<LoginUser> _userManager;
+        private readonly IConfiguration _config;
+
+        public AuthorApiController(BookClubContext context,
+            IAuthorRepository repository,
+            ILogger<AuthorApiController> logger,
+            IMapper mapper,
+            SignInManager<LoginUser> signInManager,
+            UserManager<LoginUser> userManager,
+            IConfiguration config)
+        {
+            _context = context;
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        // GET: api/Author
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            return null;
+        }
+
+        // GET: api/Author/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Author
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor()
+        {
+            // TODO: return object, or response json?
+            return null;
+        }
+
+
+        // PUT: api/Author
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuthor(int id, Author author)
+        {
+            return null;
+        }
+
+        // DELETE: api/Author/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            return null;
+        }
+
     }
 }
---
+++ BookClub/Data/Entities/Author/IAuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub.Data.Entities
+{
+    public interface IAuthorRepository
+    {
+        // TODO: Add methods to handle data from new data model
+        UserAuthor GetAllUserAuthors();
+        IEnumerable<Author> GetAllAuthors();
+
+        bool SaveAll();
+        void AddEntity(object model);
+    }
+}
