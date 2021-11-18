b07f178737a2a693cafb3ca525986846c8ac6574
nickguerra <nickg@maliciousgames.com>
2021-11-18 05:29:12 -0600

    Added Author Api Model

--- BookClub/Apis/AuthorList.ApiModel.cs
+++ BookClub/Apis/AuthorList.ApiModel.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using BookClub.Data.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookClub.Apis
 {
     public class AuthorListApiModel
     {
-        
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int? AuthorBioId { get; set; }
+        public AuthorBio AuthorBio { get; set; }
+        public ICollection<UserAuthor> UserAuthors { get; set; }
+        public ICollection<BookAuthor> BookAuthors { get; set; }
+        public ICollection<AuthorGenre> GenreAuthors { get; set; }
     }
 }
--- BookClub/Apis/AuthorList.cs
+++ BookClub/Apis/AuthorList.cs
@@ -1,8 +1,7 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using BookClub.Data.Entities;
+using BookClub.Generics;
+using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,15 +9,22 @@ namespace BookClub.Apis
 {
     public class AuthorListHandler : IRequestHandler<AuthorListRequest, AuthorListResponse>
     {
-        public AuthorListHandler()
-        {
+        private readonly IMapper _mapper;
+        private readonly IRepository<Author> _repo;
 
+        public AuthorListHandler(IMapper mapper,
+            IRepository<Author> repo)
+        {
+            _mapper = mapper;
+            _repo = repo;
         }
-       
-        public Task<AuthorListResponse> Handle(AuthorListRequest request,
+
+        public async Task<AuthorListResponse> Handle(AuthorListRequest request,
                                         CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var authors = await _repo.ListAsync(cancellationToken);
+
+            return _mapper.Map<AuthorListResponse>(authors);
         }
     }
 }
