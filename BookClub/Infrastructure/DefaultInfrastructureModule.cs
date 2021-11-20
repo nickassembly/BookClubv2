b6451b765cbcf61a71fb16da763cfd549ef6b6a9
nickguerra <nickg@maliciousgames.com>
2021-11-20 05:06:24 -0600

    Added infrastructure class

--- BookClub/Infrastructure/DefaultInfrastructureModule.cs
+++ BookClub/Infrastructure/DefaultInfrastructureModule.cs
@@ -6,6 +6,8 @@ namespace BookClub.Infrastructure
 {
     public class DefaultInfrastructureModule : Module
     {
+        // TODO: Resolve Services issue
+
         private readonly bool _isDevelopment = false;
         private readonly List<Assembly> _assemblies = new List<Assembly>();
 
