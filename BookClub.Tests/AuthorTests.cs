a7d8d5ba73a7e8839f6e5c324248b84fdb68ed48
nickguerra <nickg@maliciousgames.com>
2021-12-23 05:46:17 -0600

    Added test project

---
+++ BookClub.Tests/AuthorTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace BookClub.Tests
+{
+    public class AuthorTests
+    {
+        // TODO: Add Functional Tests for CRUD
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
