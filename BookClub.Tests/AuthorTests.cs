912cdba98ea84e421336dfdab9c1887980041f68
nickguerra <nickg@maliciousgames.com>
2021-12-25 05:33:06 -0600

    Removed intial test code

--- BookClub.Tests/AuthorTests.cs
+++ BookClub.Tests/AuthorTests.cs
@@ -10,27 +10,14 @@ namespace BookClub.Tests
 {
     public class AuthorTests
     {
-     
+
         [Fact]
         public void ReturnsListOfAuthors()
         {
-            // TODO: Get Logged in User through method
-            string userId = "7c173ffb-d6b4-4ace-aca7-db8cdcfcaf16";
+          // TODO: Functional tests for Repo Wrapper pattern
 
-            // Function should return this list, use as mock
-            List<AuthorViewModel> authorViewModels = new List<AuthorViewModel>
-            {
-                new AuthorViewModel() { },
-                new AuthorViewModel() { },
-                new AuthorViewModel() { },
-                new AuthorViewModel() { },
-                new AuthorViewModel() { },
-            };
 
-            var respository = new Mock<RepositoryWrapper>();
 
-            respository.Setup(x => x.AuthorUserRepo.ListByCondition(y => y.UserId == userId)).Returns((List<int> authorIds) => )    
-            
         }
     }
 }
