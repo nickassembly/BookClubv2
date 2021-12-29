5410eedea1fd55368e9fe37888eba1c89b435de6
Joe Guerra <joe.guerra@globalstar.com>
2021-12-29 11:00:56 -0600

    Added userbook delete functionality

--- BookClub/Controllers/BookController.cs
+++ BookClub/Controllers/BookController.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookClub.Controllers
 {
@@ -44,6 +45,31 @@ namespace BookClub.Controllers
         }
 
         [HttpGet]
+        public IActionResult UserBookDelete(BookViewModel book)
+        {
+            try
+            {
+                ClaimsPrincipal currentUser = this.User;
+                var currentUserId = UserUtils.GetLoggedInUser(currentUser);
+                var bookToDelete = _context.Books.Where(bk => bk.IdentifierType == book.IdentifierType && bk.Identifier == book.Identifier).FirstOrDefault();
+                var userBook = _context.UserBooks.Where(u => u.BookId == bookToDelete.Id && u.UserId == currentUserId).FirstOrDefault();
+
+                if (userBook != null)
+                {
+                    _context.UserBooks.Remove(userBook);
+                    _context.SaveChanges();
+                    return RedirectToAction("UserBookList");
+                }
+                return RedirectToAction("UserBookList");
+            } 
+            catch(Exception ex)
+            {
+                _logger.LogError($"Delete failed for user's book - Exception: {ex}");
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet]
         public async Task<IActionResult> UserBookList()
         {
             if (!this.User.Identity.IsAuthenticated)
@@ -65,12 +91,14 @@ namespace BookClub.Controllers
                         .ListByCondition(userBook => userBook.BookId == bookId)
                         .Select(userBook => userBook.Book).FirstOrDefaultAsync();
 
+
                     // List<int> authorBooksIds = await _context.BookAuthors.Where(x => x.AuthorId == authorId).Select(y => y.BookId).ToListAsync();
 
                     var bookAuthorIds = await _repoWrapper.UserBookRepo
                         .ListByCondition(userBook => userBook.BookId == bookId)
                         .Select(authorBook => authorBook.BookId).ToListAsync();
 
+                    List<Book> plainBooks = _context.Books.Where(b => bookAuthorIds.Contains(b.Id)).ToList();
                     List<Author> authorBooks = _context.Authors.Where(author => bookAuthorIds.Contains(author.Id)).ToList();
 
                     // List<int> authorGenreIds = await _context.GenreAuthors.Where(x => x.AuthorId == authorId).Select(y => y.GenreId).ToListAsync();
@@ -98,25 +126,76 @@ namespace BookClub.Controllers
             }
         }
 
+        private List<SelectListItem> GetGenresForSelectList()
+        {
+            var genres = _context.Genres.ToList();
+
+            var genreListItem = genres.Select(genre => new SelectListItem { Text = genre.GenreName, Value = genre.Id.ToString() }).ToList();
+
+            return genreListItem;
+        }
+
+        private List<SelectListItem> GetAuthorsForSelectList()
+        {
+            var authors = _context.Authors.ToList();
+
+            var authorListItem = authors.Select(author => new SelectListItem { Text = author.Firstname + author.Lastname, Value = author.Firstname + author.Lastname }).ToList();
+
+            return authorListItem;
+        }
 
         public async Task<IActionResult> AddNewBookForUser([FromBody] BookViewModel bookVM)
         {
-            if (!this.User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Account");
+            if (!this.User.Identity.IsAuthenticated) return RedirectToAction("Login", "Account");
+            if (!this.ModelState.Any())
+            {
+                bookVM.GenreList = GetGenresForSelectList();
+                bookVM.AuthorList = GetAuthorsForSelectList();
+
+                return View(bookVM);
+            }
 
             Book book = _mapper.Map<Book>(bookVM);
-
             try
             {
-                var currentUserId = UserUtils.GetLoggedInUser(this.User);
+                var existingBook = _context.Books.Where(
+                                        findBook => book.Identifier == findBook.Identifier
+                                        && book.IdentifierType == findBook.IdentifierType
+                                        && findBook.Identifier != null).FirstOrDefault();
+                if (existingBook != null)
+                {
+                    _logger.LogInformation($" Book already exists: {existingBook.IdentifierType} : {existingBook.Identifier} ");
+                    return RedirectToAction("UserBookList");
+                }
 
+                var currentUserId = UserUtils.GetLoggedInUser(this.User);
+                
                 var bookToAdd = await _context.Books.AddAsync(book);
                 await _context.SaveChangesAsync();
 
                 var addedBook = await _repoWrapper.BookRepo.ListByCondition(book => book.Id == bookToAdd.Entity.Id).FirstOrDefaultAsync();
 
-
                 _context.UserBooks.Add(new UserBook { BookId = addedBook.Id, UserId = currentUserId });
+
+
+                List<int> bookGenreIds = bookVM.GenreIds;
+                List<int> authorBookIds = bookVM.AuthorIds;
+
+                if (bookGenreIds != null)
+                {
+                    foreach (var genreId in bookGenreIds)
+                    {
+                        _context.GenreBooks.Add(new BookGenre { BookId = addedBook.Id, GenreId = genreId });
+                    }
+                }
+
+                if (authorBookIds != null)
+                {
+                    foreach (var authorId in authorBookIds)
+                    {
+                        _context.BookAuthors.Add(new BookAuthor { BookId = addedBook.Id, AuthorId = authorId });
+                    }
+                }
 
                 await _context.SaveChangesAsync();
 
