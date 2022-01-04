aca9aca437a165605806604df5f2fec2f6339a33
nickguerra <nickg@maliciousgames.com>
2022-01-03 19:29:30 -0600

    Started writing tests

--- BookClub.Tests/AuthorTests.cs
+++ BookClub.Tests/AuthorTests.cs
@@ -1,3 +1,8 @@
+using BookClub.Controllers;
+using BookClub.Core.IConfiguration;
+using BookClub.Core.Repositories;
+using BookClub.Data.Entities;
+using Moq;
 using Xunit;
 
 namespace BookClub.Tests
@@ -8,7 +13,15 @@ namespace BookClub.Tests
         [Fact]
         public void ReturnsListOfAuthors()
         {
-          
+            //bool searchWasCalled = false; // ??
+
+            //Mock<IGenericRepository<Author>> repositoryMock = new Mock<IGenericRepository<Author>>();
+            //repositoryMock.Setup(author => author.All()).Callback(() => searchWasCalled = true);
+
+            //var mockUnitOfWork = new Mock<IUnitOfWork>();
+            //mockUnitOfWork.Setup(uow => uow.Authors).Returns(repositoryMock.Object);
+
+ 
         }
 
         [Fact]
