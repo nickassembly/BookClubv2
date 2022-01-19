7d3fc871db8e47a123654951dc918b68e89a2dd9
nickguerra <nickg@maliciousgames.com>
2022-01-19 05:54:54 -0600

    Added interface for sending email

---
+++ BookClub/Utils/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub.Utils
+{
+    public class EmailService : IEmailService
+    {
+        public List<string> GetEmailRecepients(string connectionString)
+        {
+            // DB call to get user list or logged in user's friends list
+            throw new NotImplementedException();
+        }
+
+        public void SendMail(List<string> emailRecepients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookClub/Utils/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub.Utils
+{
+    public interface IEmailService
+    {
+        List<string> GetEmailRecepients(string connectionString);
+        void SendMail(List<string> emailRecepients);
+    }
+}
