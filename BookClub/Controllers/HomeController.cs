75922d011e13754d2152c162a4bd5a3c2f62a889
nickguerra <nickg@maliciousgames.com>
2022-04-01 05:33:09 -0500

    Started populating the UserProfile model for Index list

--- BookClub/Controllers/HomeController.cs
+++ BookClub/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using BookClub.Data.Entities.User;
+﻿using BookClub.Data;
+using BookClub.Data.Entities;
+using BookClub.Data.Entities.User;
+using BookClub.Utils;
 using BookClub.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace BookClub.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BookClubContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,15 +23,32 @@ namespace BookClub.Controllers
 
         public IActionResult Index()
         {
-            // TODO: Need to populate a LoginUser Profile and send
-
             if (!this.User.Identity.IsAuthenticated)
                 return RedirectToAction("Login", "Account");
 
+            var userId = UserUtils.GetLoggedInUser(this.User);
+
+            // TODO: Extract to Method, use Automapper to create VMS, return VMS to this method to hydrate model
+            var userBookIds = _context.UserBooks.Where(x => x.UserId == userId).Select(x => x.BookId);
+
+            var userBooks = _context.Books.Where(b => userBookIds.Contains(b.Id));
+
+            List<BookViewModel> userBookList = new();
+
+            foreach (var book in userBooks)
+            {
+                BookViewModel bookVM = new BookViewModel
+                {
+
+                };
+
+                userBookList.Add(bookVM);
+            }
+
             LoginUserProfileViewModel loginProfile = new LoginUserProfileViewModel
             {
-                UserBookList = new List<BookViewModel>(), // Get users booklist here
-                Friends = new List<LoginUserFriendship>() // Get user friends here
+                UserBookList = userBookList,
+               // Friends = userFriendsList
             };
 
             return View(loginProfile);
